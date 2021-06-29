64809c5ed5335629e8795fb7d6131bb3392c3ff5
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-06-28 21:03:42 -0700

    commit

--- facebook_collect.py
+++ facebook_collect.py
@@ -13,6 +13,7 @@ import facebook_to_album
 import facebook_scraper
 import plain_db
 import random
+import time
 
 with open('credential') as f:
 	credential = yaml.load(f, Loader=yaml.FullLoader)
@@ -24,50 +25,46 @@ existing = plain_db.loadKeyOnlyDB('existing')
 tele = Updater(credential['bot_token'], use_context=True)
 debug_group = tele.bot.get_chat(credential['debug_group'])
 blocklist = plain_db.loadKeyOnlyDB('blocklist')
+fetchtime = plain_db.load('fetchtime')
 
 def getKey(url):
 	return url.strip('/').split('/')[-1]
 
+def getSchedule():
+	schedules = []
+	for channel_id, pages in setting.items():
+		for page, detail in pages.items():
+			schedules.append((channel_id, pages.items()
+			if fetchtime.get(page) > time.time() - 24 * 60 * 60:
+				continue
+	fetchtime.update(page, int(time.time()))
+	return tele.bot.get_chat(channel_id), page, detail
+
 @log_on_fail(debug_group)
 def run():
-	sent = False
-	for channel_id, pages in setting.items():
-		channel = tele.bot.get_chat(channel_id)
-		schedule = list(pages.items())
-		random.shuffle(schedule)
-		for page, detail in schedule[:1]:
-			posts = facebook_scraper.get_posts(page, pages=10)
-			count = 0
-			for post in posts:
-				count += 1
-				url = post['post_url']
-				with open('nohup.out', 'a') as f:
-					f.write('%s\n%s\n\n' % (url, str(post)))
-				if existing.contain(url):
-					continue
-				if getKey(url) in [getKey(item) for item in existing._db.items.keys()]:
-					continue
-				if post['likes'] < detail.get('likes', 100):
-					continue
-				if matchKey(str(post), blocklist.items()):
-					continue
-				album = facebook_to_album.get(post, detail)
-				if not sent:
-					sent = True
-				else:
-					item_len = len(album.imgs) or 1
-					time.sleep(item_len * item_len + 5 * item_len)
-				try:
-					album_sender.send_v2(channel, album)
-				except Exception as e:
-					print('facebook sending fail', url, e)
-					with open('nohup.out', 'a') as f:
-						f.write('\n%s %s %s' % (url, str(e), str(post)))
-					continue
-				existing.add(album.url)
-			if count == 0:
-				print('facebook fetch fail', page, count)
-				return
+	channel, page, detail = getSchedule()
+	posts = facebook_scraper.get_posts(page, pages=10)
+	for post in posts:
+		url = post['post_url']
+		with open('nohup.out', 'a') as f:
+			f.write('%s\n%s\n\n' % (url, str(post)))
+		if existing.contain(url):
+			continue
+		if getKey(url) in [getKey(item) for item in existing._db.items.keys()]:
+			continue
+		if post['likes'] < detail.get('likes', 100):
+			continue
+		if matchKey(str(post), blocklist.items()):
+			continue
+		album = facebook_to_album.get(post, detail)
+		try:
+			album_sender.send_v2(channel, album)
+		except Exception as e:
+			print('facebook sending fail', url, e)
+			with open('nohup.out', 'a') as f:
+				f.write('\n%s %s %s' % (url, str(e), str(post)))
+			continue
+		existing.add(album.url)
 		
 if __name__ == '__main__':
 	run()
