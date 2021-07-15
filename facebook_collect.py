0c948051d61ffbe260fab9e4bbc00a81f48031fc
Yunzhi Gao <gaoyunzhi@gmail.com>
2021-07-14 19:40:48 -0700

    commit

--- facebook_collect.py
+++ facebook_collect.py
@@ -16,10 +16,10 @@ import random
 import time
 
 with open('credential') as f:
-	credential = yaml.load(f, Loader=yaml.FullLoader)
+    credential = yaml.load(f, Loader=yaml.FullLoader)
 
 with open('db/setting') as f:
-	setting = yaml.load(f, Loader=yaml.FullLoader)
+    setting = yaml.load(f, Loader=yaml.FullLoader)
 
 existing = plain_db.loadKeyOnlyDB('existing')
 tele = Updater(credential['bot_token'], use_context=True)
@@ -28,49 +28,55 @@ blocklist = plain_db.loadKeyOnlyDB('blocklist')
 fetchtime = plain_db.load('fetchtime')
 
 def getKey(url):
-	return url.strip('/').split('/')[-1]
+    return url.strip('/').split('/')[-1]
 
 def getSchedule():
-	schedules = []
-	for channel_id, pages in setting.items():
-		for page, detail in pages.items():
-			schedules.append((fetchtime.get(page, 0), channel_id, page, detail))
-	schedules.sort()
-	_, channel_id, page, detail = schedules[0]
-	fetchtime.update(page, int(time.time()))
-	return tele.bot.get_chat(channel_id), page, detail
+    schedules = []
+    for channel_id, pages in setting.items():
+        for page, detail in pages.items():
+            schedules.append((fetchtime.get(page, 0), channel_id, page, detail))
+    schedules.sort()
+    if time.time() - schedules[-1][0] < 30 * 60:
+        return
+    _, channel_id, page, detail = schedules[0]
+    fetchtime.update(page, int(time.time()))
+    return tele.bot.get_chat(channel_id), page, detail
 
 @log_on_fail(debug_group)
 def run():
-	channel, page, detail = getSchedule()
-	try:
-		posts = facebook_scraper.get_posts(page, pages=10)
-	except Exception as e:
-		message = 'facebook fetch failed for %s: %s' % (page, e)
+    schedule = getSchedule()
+    if not schedule:
+        print('facebook skip, min_interval: 30 minutes')
+        return
+    channel, page, detail = schedule
+    try:
+        posts = facebook_scraper.get_posts(page, pages=10)
+    except Exception as e:
+        message = 'facebook fetch failed for %s: %s' % (page, e)
         print(message)
         debug_group.send_message(message)
         return
-	for post in posts:
-		url = post['post_url']
-		with open('nohup.out', 'a') as f:
-			f.write('%s\n%s\n\n' % (url, str(post)))
-		if existing.contain(url):
-			continue
-		if getKey(url) in [getKey(item) for item in existing._db.items.keys()]:
-			continue
-		if post['likes'] < detail.get('likes', 100):
-			continue
-		if matchKey(str(post), blocklist.items()):
-			continue
-		album = facebook_to_album.get(post, detail)
-		try:
-			album_sender.send_v2(channel, album)
-		except Exception as e:
-			print('facebook sending fail', url, e)
-			with open('nohup.out', 'a') as f:
-				f.write('\n%s %s %s' % (url, str(e), str(post)))
-			continue
-		existing.add(album.url)
-		
+    for post in posts:
+        url = post['post_url']
+        with open('nohup.out', 'a') as f:
+            f.write('%s\n%s\n\n' % (url, str(post)))
+        if existing.contain(url):
+            continue
+        if getKey(url) in [getKey(item) for item in existing._db.items.keys()]:
+            continue
+        if post['likes'] < detail.get('likes', 100):
+            continue
+        if matchKey(str(post), blocklist.items()):
+            continue
+        album = facebook_to_album.get(post, detail)
+        try:
+            album_sender.send_v2(channel, album)
+        except Exception as e:
+            print('facebook sending fail', url, e)
+            with open('nohup.out', 'a') as f:
+                f.write('\n%s %s %s' % (url, str(e), str(post)))
+            continue
+        existing.add(album.url)
+        
 if __name__ == '__main__':
-	run()
+    run()
